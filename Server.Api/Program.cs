390e7577c2d929aae556253fee9800bcc8cf8228
jpandanw <jmspaul101w@gmail.com>
2025-09-03 14:29:28 +0800

    Created Server.Api.csproj

---
+++ Server.Api/Program.cs
@@ -0,0 +1,34 @@
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddMigrationService(connectionString ?? "");
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    serviceProvider.MigrationServiceUp();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
